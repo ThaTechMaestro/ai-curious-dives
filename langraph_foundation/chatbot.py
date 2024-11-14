ad940d74b3c7737d4f9abdd02ad43ff4940c07ac
ThaTechMaestro <muse.pearls@gmail.com>
2024-11-14 22:38:51 +0000

    adds extra stuff

--- langraph_foundation/chatbot.py
+++ langraph_foundation/chatbot.py
@@ -2,6 +2,7 @@ from typing import Annotated
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
+from langchain_anthropic import ChatAnthropic
 
 
 class State(TypedDict):
@@ -12,3 +13,15 @@ class State(TypedDict):
 
 
 graph_builder = StateGraph(State)
+
+llm = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+
+def chatbot(state: State):
+    return {
+        "messages": [llm.invoke(state["messages"])]
+    }
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+graph = graph_builder.compile()
